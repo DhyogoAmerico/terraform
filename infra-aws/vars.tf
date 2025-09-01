1d18a02b89ebd70a0b997c28b6fe01318a397825
DhyogoSiqueira <9dhyogoamerico@gmail.com>
2025-09-01 08:33:54 -0300

    success create infra aws

--- infra-aws/vars.tf
+++ infra-aws/vars.tf
@@ -16,3 +16,23 @@ variable "tags" {
     Environment = "Dev"
   }
 }
+
+variable "instance_types" {
+  type    = list(string)
+  default = ["t3.medium"]
+}
+
+variable "LabRole" {
+  description = "The IAM role to be assumed by the lab user"
+  default     = "arn:aws:iam::905236634338:role/LabRole"
+}
+
+variable "user_arn" {
+  description = "The ARN of the user to be granted access"
+  default     = "arn:aws:iam::905236634338:role/voclabs"
+}
+
+variable "policy_associate_arn" {
+  description = "The ARN of the policy to be associated with the user"
+  default     = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+}
